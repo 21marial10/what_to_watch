c3b47d298d9ad1811154e1ef5de8489b5c5d026c
Maria Levina <21marial10@gmail.com>
2026-04-26 23:51:16 +0300

    commit

--- opinions_app/forms.py
+++ opinions_app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
+from flask_wtf.file import FileAllowed, MultipleFileField
 
 
 class OpinionForm(FlaskForm):
@@ -17,4 +18,17 @@ class OpinionForm(FlaskForm):
         'Добавьте ссылку на подробный обзор фильма',
         validators=[Length(1, 256), Optional()]
     )
+    # Добавьте новое поле в форму.
+    images = MultipleFileField(
+        validators=[
+            FileAllowed(
+                ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
+                message=(
+                    'Выберите файлы с расширением '
+                    '.jpg, .jpeg, .png, .gif или .bmp'
+                )
+            )
+        ]
+    )
+
     submit = SubmitField('Добавить')
